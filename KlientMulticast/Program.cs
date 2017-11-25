29e06d5718bd67fa86d90a6e665bcb827d633e5a
Piotrek <piotrek120017@gmail.com>
2017-11-26 00:11:05 +0100

    initial commit

---
+++ KlientMulticast/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace KlientMulticast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient client = new UdpClient();
+
+            client.ExclusiveAddressUse = false;
+            IPEndPoint localEp = new IPEndPoint(IPAddress.Any, 2222);
+            client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            client.ExclusiveAddressUse = false;
+
+            client.Client.Bind(localEp);
+
+            IPAddress multicastAddr = IPAddress.Parse("239.0.0.222");
+            client.JoinMulticastGroup(multicastAddr);
+            while (true)
+            {
+                byte[] dataByte = client.Receive(ref localEp);
+                string data = Encoding.UTF8.GetString(dataByte);
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
